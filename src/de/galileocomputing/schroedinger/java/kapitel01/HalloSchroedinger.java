d39c189b9dd46e68a4937ed4ac0f88bdcee9ae47
Razor-Bob <razorbob007@googlemail.com>
2024-10-14 19:06:24 +0200

    Initial Commit.

---
+++ src/de/galileocomputing/schroedinger/java/kapitel01/HalloSchroedinger.java
@@ -0,0 +1,15 @@
+package de.galileocomputing.schroedinger.java.kapitel01;
+
+public class HalloSchroedinger {
+
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hallo " + args[0]);
+
+	}
+
+}
