34c111cb98c97e3a09e892e0abf1687c9db4edbd
Razor-Bob <razorbob007@googlemail.com>
2024-10-15 22:57:28 +0200

    Create AusgabenRaetsel.java

---
+++ src/de/galileocomputing/schroedinger/java/kapitel02/AusgabenRaetsel.java
@@ -0,0 +1,18 @@
+package de.galileocomputing.schroedinger.java.kapitel02;
+
+public class AusgabenRaetsel {
+
+	public static void main(String[] args) {
+		int zahl = 7;
+		System.out.println(++zahl); // 3
+		System.out.println(zahl++); // 2
+		System.out.println(zahl * zahl); // 1
+		System.out.println(zahl = zahl * zahl); // 4
+		System.out.println(zahl = (zahl - 9) / 8); // 7
+		System.out.println(zahl--); // 6
+		System.out.println(--zahl); // 5
+		System.out.println(zahl); //
+
+	}
+
+}
