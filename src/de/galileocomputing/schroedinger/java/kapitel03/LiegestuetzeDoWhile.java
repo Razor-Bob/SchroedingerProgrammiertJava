bab7df461506ef0e32116e9fb71a471c49cae15f
Razor-Bob <razorbob007@googlemail.com>
2025-01-15 08:54:11 +0100

    Create LiegestuetzeDoWhile.java
    Fünfzig Liegestütze mit der do-while-Schleife

---
+++ src/de/galileocomputing/schroedinger/java/kapitel03/LiegestuetzeDoWhile.java
@@ -0,0 +1,15 @@
+package de.galileocomputing.schroedinger.java.kapitel03;
+
+public class LiegestuetzeDoWhile {
+
+	public static void main(String[] args) {
+		// Fünfzig Liegestütze mit der do-while-Schleife
+		
+		int i = 0;
+		do {
+			System.out.println(++i);
+		} while (i < 50);
+
+	}
+
+}
