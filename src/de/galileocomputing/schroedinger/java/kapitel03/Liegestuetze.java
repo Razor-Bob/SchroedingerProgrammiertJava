ba3ffe5b7fe9d0a697026dca3c88cebcb5723c51
Razor-Bob <razorbob007@googlemail.com>
2025-01-15 08:46:35 +0100

    Create Liegestuetze.java
    Fünfzig Liegestütze mit der for-Schleife

---
+++ src/de/galileocomputing/schroedinger/java/kapitel03/Liegestuetze.java
@@ -0,0 +1,14 @@
+package de.galileocomputing.schroedinger.java.kapitel03;
+
+public class Liegestuetze {
+
+	public static void main(String[] args) {
+		// Fünfzig Liegestütze mit der for-Schleife
+		
+		for (int i = 0; i < 50; i++) {
+			System.out.println(i + 1);
+		}
+
+	}
+
+}
