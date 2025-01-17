22dbf8d30e69b9352d542951538a166691f983a7
Razor-Bob <razorbob007@googlemail.com>
2025-01-17 09:30:17 +0100

    Create ZahlenInUmgekehrterReihenfolgeAusgeben.java
    Gib die Zahlen 1 bis 10 in umgekehrter Reihenfolge aus (Seite 118)

---
+++ src/de/galileocomputing/schroedinger/java/kapitel03/ZahlenInUmgekehrterReihenfolgeAusgeben.java
@@ -0,0 +1,13 @@
+package de.galileocomputing.schroedinger.java.kapitel03;
+
+public class ZahlenInUmgekehrterReihenfolgeAusgeben {
+
+	public static void main(String[] args) {
+		// Gib die Zahlen 1 bis 10 in umgekehrter Reihenfolge aus
+		for (int i = 10; i > 0; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
