25a19358969bc76bfc9ca299310c716711cabf9c
Razor-Bob <razorbob007@googlemail.com>
2025-01-17 09:53:46 +0100

    Primzahlen
    PrimzahlenBerechner (Seite 119) &
    
    PrimzahlenBerechnerOptimiert (Seite 120)

---
+++ src/de/galileocomputing/schroedinger/java/kapitel03/PrimzahlenBerechner.java
@@ -0,0 +1,27 @@
+package de.galileocomputing.schroedinger.java.kapitel03;
+
+public class PrimzahlenBerechner {
+
+	public static void main(String[] args) {
+		// Betrachte alle Zahlen i von 1 bis 100
+		for(int i=1; i<=100; i++) {
+			// Annahme: i ist Primzahl
+			boolean istPrimzahl = true;
+			// Betrachte alle Zahlen j von 2 bis i-1
+			for(int j=2; j<i; j++) {
+				// Wenn i durch j geteilt werden kann
+				if(i%j == 0) {
+					// i ist doch keine Primzahl
+					istPrimzahl = false;
+					// und die Teilersuche kann abgebrochen werden
+					break;
+				}
+			}
+			if(istPrimzahl) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
