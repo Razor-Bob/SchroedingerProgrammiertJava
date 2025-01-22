38130abe9cddd557503215ff8f64b7c99486e956
Razor-Bob <razorbob007@googlemail.com>
2025-01-22 15:49:00 +0100

    Create WoWTextadventure2IfElseSchleife.java
    WoWTextadventure2 Kronenfund mit if-else-Lösung (Seite 124)

---
+++ src/de/galileocomputing/schroedinger/java/kapitel03/WoWTextadventure2IfElseSchleife.java
@@ -0,0 +1,10 @@
+package de.galileocomputing.schroedinger.java.kapitel03;
+
+public class WoWTextadventure2IfElseSchleife {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
